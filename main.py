eae83a325eea61d3840c046058b35f2618b2732d
martynasmat <martynasmat03@gmail.com>
2025-03-26 20:43:15 -0400

    Get BSSID & SSID from beacon frames

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from scapy.all import *
-import scapy
+from scapy.layers.dot11 import *
 
 def handle_beacon(pkt):
 
     # Filter beacon frames
-    if pkt.haslayer(scapy.layers.dot11.Dot11Beacon):
-        print('yes')
+    # 802.11 specifies management frames as type 0 and beacon frames as subtype 8
+    if pkt.haslayer(Dot11Beacon) and pkt[Dot11].type == 0 and pkt[Dot11].subtype == 8:
+        bssid = pkt[Dot11].addr3
+        ssid = pkt[Dot11Elt].info.decode()
+        print(f'BSSID: {bssid} SSID: {ssid}')
 
 
 sniff(iface="wlan0mon", prn=handle_beacon, store=0)
