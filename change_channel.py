ff4c6ebcb17fed1e32302939230d18ba6ed2d577
martynasmat <martynasmat03@gmail.com>
2025-03-27 21:08:28 -0400

    Move change_channel to change_channel.py

---
+++ change_channel.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+def change_channel(channel_arg, interface):
+    # Periodically change channels to discover all available networks
+    channel = channel_arg
+
+    while True:
+        print(f'Switched channel to channel {channel}')
+        os.system(f'sudo iwconfig {interface} channel {channel}')
+        # 2.4GHz WiFi supports 14 channels according to https://en.wikipedia.org/wiki/List_of_WLAN_channels
+        channel = channel % 14 + 1
+        time.sleep(2)
--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 from scapy.all import *
 from scapy.layers.dot11 import *
 import threading
-import os
+from change_channel import change_channel
 
 INTERFACE_NAME = 'wlan0mon'
 START_CHANNEL = 1
 
-def change_channel():
-    # Periodically change channels to discover all available networks
-    channel = START_CHANNEL
-
-    while True:
-        print(f'Switched channel to channel {channel}')
-        os.system(f'sudo iwconfig {INTERFACE_NAME} channel {channel}')
-        # 2.4GHz WiFi supports 14 channels according to https://en.wikipedia.org/wiki/List_of_WLAN_channels
-        channel = channel % 14 + 1
-        time.sleep(2)
-
 
 def handle_beacon(pkt):
-
     # Filter beacon frames
     # 802.11 specifies management frames as type 0 and beacon frames as subtype 8
     if pkt.haslayer(Dot11Beacon) and pkt[Dot11].type == 0 and pkt[Dot11].subtype == 8:
@@ -29,7 +17,7 @@ def handle_beacon(pkt):
 
 
 print('Starting channel change thread')
-channel_thread = threading.Thread(target=change_channel)
+channel_thread = threading.Thread(target=change_channel, args=(START_CHANNEL, INTERFACE_NAME))
 channel_thread.start()
 
 print('Sniffing...')
