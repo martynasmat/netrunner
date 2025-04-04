f59d0f440b97222b4ee87def080457ef01c50e5d
martynasmat <martynasmat03@gmail.com>
2025-04-03 21:32:48 -0400

    Handle probe requests, create SSID map

--- main.py
+++ main.py
@@ -9,22 +9,33 @@ START_CHANNEL = 1
 
 class AccessPoint():
     
-    def __init__(self, ssid, bssid):
+    def __init__(self, ssid, bssid, signal_strength=None):
         self.ssid = ssid
         self.bssid = bssid
         # List of clients (possibly) connected to the AP
         self.clients = []
+        self.signal_strength = signal_strength
+
+    def update_signal_strength(self, dbm):
+        self.signal_strength = dbm
+
+    def add_client(self, client):
+        self.clients.append(client)
 
 
 def handle_beacon(pkt):
     bssid = pkt[Dot11].addr3
     ssid = pkt[Dot11Elt].info.decode()
+    signal_strength = pkt[RadioTap].dBm_AntSignal
 
     if bssid not in ap_bssids:
         ap_bssids.add(bssid)
-        ap = AccessPoint(ssid, bssid)
+        ap = AccessPoint(ssid, bssid, signal_strength)
         access_points.append(ap)
-        ap_map[bssid] = ap
+        bssid_map[bssid] = ap
+        ssid_map[ssid] = ap
+    else:
+        bssid_map[bssid].update_signal_strength(signal_strength)
 
 
 def get_aps():
@@ -36,26 +47,40 @@ def handle_packet(pkt):
     if pkt.haslayer(Dot11Beacon) and pkt[Dot11].type == 0 and pkt[Dot11].subtype == 8:
         handle_beacon(pkt)
 
+    # Handle probe requests
+    elif pkt.haslayer(Dot11ProbeReq) and pkt[Dot11].type == 0 and pkt[Dot11].subtype == 4:
+        ssid = pkt[Dot11ProbeReq].info.decode()
+        if ssid:
+            ap = ssid_map[ssid]
+            if ssid not in ap.clients: 
+                ap.clients.append(pkt[Dot11ProbeReq.addr2])
+
     # Handle data frames
-    if pkt.haslayer(Dot11) and pkt[Dot11].type == 2:
+    elif pkt.haslayer(Dot11) and pkt[Dot11].type == 2:
         match pkt[Dot11].subtype:
             # Data frame
             case 0:  
                 source = pkt[Dot11].addr2
                 destination = pkt[Dot11].addr1
-                print(f'Data frame / Source {source} / Destination {destination}')
+                process_client_data_frame(source, destination)
 
             # Null data frame
             case 4:
                 source = pkt[Dot11].addr2
                 destination = pkt[Dot11].addr3
-                print(f'Null data frame / Source {source} / Destination {destination}')
+                process_client_data_frame(source, destination)
 
             # QoS data frame
             case 8:
                 source = pkt[Dot11].addr2
                 destination = pkt[Dot11].addr3
-                print(f'QoS data frame / Source {source} / Destination {destination}')
+                process_client_data_frame(source, destination)
+
+
+def process_client_data_frame(src, dest):
+    if dest in bssid_map.keys() and dest != 'ff:ff:ff:ff:ff:ff':
+            if src not in bssid_map[dest].clients:
+                bssid_map[dest].add_client(src)
 
 
 def start_sniffing():
@@ -66,7 +91,8 @@ access_points = []
 # Access point MAC addresses
 ap_bssids = set()
 # BSSID:AccessPoint
-ap_map = {}
+bssid_map = {}
+ssid_map = {}
 
 print('Starting GUI thread')
 gui_thread = threading.Thread(target=gui.start_gui, args=(get_aps,))
